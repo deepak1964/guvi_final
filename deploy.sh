75462f09063f04d5de9dd2f0c79b2729c5a27f0e
deepak1964 <127740099+deepak1964@users.noreply.github.com>
2025-05-13 14:34:48 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u thejus7 -p dckr_pat_je4Q6CnC2mD7ln_2S7HAtngJ6ng
-    docker tag test thejus7/test
-    docker push thejus7/test
-    kubectl create deployment jen --image=thejus7/test --port=80
+    docker login -u deepak102 -p dckr_pat_gda3mcUU81qsQVUZ3eJcEZensNY
+    docker tag test deepak102/test
+    docker push deepak102/test
+    kubectl create deployment jen --image=deepak102/test --port=80
      kubectl expose deployment jen --type=NodePort --port=80
