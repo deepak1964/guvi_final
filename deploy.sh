3e9256fdc877f4178d4dcf5af624a43715dd4bd2
deepak1964 <127740099+deepak1964@users.noreply.github.com>
2025-05-21 13:42:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,4 @@
     docker login -u deepak102 -p dckr_pat_gda3mcUU81qsQVUZ3eJcEZensNY
     docker tag test deepak102/test
     docker push deepak102/test
-    kubectl create deployment jen --image=deepak102/test --port=80
-     kubectl expose deployment jen --type=NodePort --port=80
+  
